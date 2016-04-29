86de9d27c93993734157c06f61e5fef9d245aa4c
surya.sankpal <surya.sankpal@YI1004596DT.Yash.com>
2016-04-29 18:59:56 +0530

    Commented

--- yashissuetrackersystemweb/src/main/java/com/yash/yits/controller/IssueController.java
+++ yashissuetrackersystemweb/src/main/java/com/yash/yits/controller/IssueController.java
@@ -5,12 +5,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import jdk.nashorn.internal.ir.RuntimeNode.Request;
+//import jdk.nashorn.internal.ir.RuntimeNode.Request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
